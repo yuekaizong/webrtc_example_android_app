36b2abe4a6241706291a09a03ece7d3afcbcb6ce
yuekaizong <596760835@qq.com>
2022-01-04 18:30:42 +0800

    增加二进制文件传输

--- app/src/main/java/aga/songmaya/support/utils/FileExt.java
+++ app/src/main/java/aga/songmaya/support/utils/FileExt.java
@@ -2,8 +2,12 @@ package aga.songmaya.support.utils;
 
 import android.app.Activity;
 import android.app.Fragment;
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -11,6 +15,7 @@ import org.apache.commons.io.IOUtils;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -64,10 +69,44 @@ public class FileExt {
         try (OutputStream out = new FileOutputStream(filename)) {
             out.write(data);
             out.close();
+            saveImage2Album(activity, new File(filename));
             Log.e(TAG, "save path:" + filename);
         } catch (Exception e) {
             Log.e(TAG, "save fail", e);
         }
+    }
+
+    public static void saveImage2Album(Context context, File mediaFile) {
+        Uri uri = Uri.fromFile(mediaFile);
+        context.sendBroadcast(new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE, uri));
+
+        try {
+            MediaStore.Images.Media.insertImage(context.getContentResolver(),
+                    mediaFile.getAbsolutePath(), mediaFile.getName(), null);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void saveVideo2Album(Context context, File file) {
+        ContentResolver localContentResolver = context.getContentResolver();
+        ContentValues localContentValues = getVideoContentValues(context, file, System.currentTimeMillis());
+        Uri localUri = localContentResolver.insert(MediaStore.Video.Media.EXTERNAL_CONTENT_URI, localContentValues);
+        context.sendBroadcast(new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE, localUri));
+
+    }
+
+    public static ContentValues getVideoContentValues(Context paramContext, File paramFile, long paramLong) {
+        ContentValues localContentValues = new ContentValues();
+        localContentValues.put(MediaStore.Images.Media.TITLE, paramFile.getName());
+        localContentValues.put(MediaStore.Images.Media.DISPLAY_NAME, paramFile.getName());
+        localContentValues.put(MediaStore.Images.Media.MIME_TYPE, "video/mp4");
+        localContentValues.put(MediaStore.Images.Media.DATE_TAKEN, Long.valueOf(paramLong));
+        localContentValues.put(MediaStore.Images.Media.DATE_MODIFIED, Long.valueOf(paramLong));
+        localContentValues.put(MediaStore.Images.Media.DATE_ADDED, Long.valueOf(paramLong));
+        localContentValues.put(MediaStore.Images.Media.DATA, paramFile.getAbsolutePath());
+        localContentValues.put(MediaStore.Images.Media.SIZE, Long.valueOf(paramFile.length()));
+        return localContentValues;
     }
 
 
